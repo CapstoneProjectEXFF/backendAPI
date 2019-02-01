c27832e20cb7af28290ab3f3999118115656bdec
decenova <maiducthang97@gmail.com>
2019-02-01 23:27:03 +0700

    Add friend: create relation entity

--- src/main/java/com/capstone/exff/ExffApplication.java
+++ src/main/java/com/capstone/exff/ExffApplication.java
@@ -5,9 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 
 @SpringBootApplication
@@ -16,7 +13,6 @@ public class ExffApplication {
     public static void main(String[] args) {
         SpringApplication.run(ExffApplication.class, args);
     }
-
     @Bean
     public FilterRegistrationBean<JwtFilter> loggingFilter(){
         FilterRegistrationBean registrationBean
@@ -28,15 +24,5 @@ public class ExffApplication {
 
         return registrationBean;
     }
-
-//    @Bean
-//    public WebMvcConfigurer corsConfigurer() {
-//        return new WebMvcConfigurerAdapter() {
-//            @Override
-//            public void addCorsMappings(CorsRegistry registry) {
-//                registry.addMapping("/**");
-//            }
-//        };
-//    }
 }
 
