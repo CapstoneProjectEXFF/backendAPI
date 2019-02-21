5741371c910ac89b359deedf694459eaf1989da5
Mai Đức Thắng <maiducthang97@gmail.com>
2019-02-21 10:52:33 +0700

    fix role column name

--- src/main/java/com/capstone/exff/entities/RoleEntity.java
+++ src/main/java/com/capstone/exff/entities/RoleEntity.java
@@ -4,7 +4,7 @@ import javax.persistence.*;
 import java.util.Objects;
 
 @Entity
-@Table(name = "role", schema = "exff")
+@Table(name = "`role`", schema = "exff")
 public class RoleEntity {
     private int id;
     private String name;
