4e22fe30ead86032c103e1f3887f1af1aaf65155
decenova <maiducthang97@gmail.com>
2019-03-14 18:16:31 +0700

    done get relatitonship

--- src/main/java/com/capstone/exff/controllers/RelationshipController.java
+++ src/main/java/com/capstone/exff/controllers/RelationshipController.java
@@ -5,11 +5,14 @@ import com.capstone.exff.entities.UserEntity;
 import com.capstone.exff.services.RelationshipServices;
 import com.capstone.exff.utilities.ExffMessage;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -20,6 +23,23 @@ public class RelationshipController {
     @Autowired
     public RelationshipController(RelationshipServices relationshipServices) {
         this.relationshipServices = relationshipServices;
+    }
+
+    @GetMapping("/relationship")
+    public ResponseEntity getRequestAddRelationship(
+            @RequestParam(value = "page", defaultValue = "0") int page,
+            @RequestParam(value = "size", defaultValue = "10") int size,
+            @RequestAttribute("USER_INFO") UserEntity userEntity
+    ) {
+        List<RelationshipEntity> relationshipEntities;
+        try {
+            int userId = userEntity.getId();
+            Pageable pageable = PageRequest.of(page,size);
+            relationshipEntities = relationshipServices.getAddRelationshipRequest(userId, pageable);
+        } catch (Exception e) {
+            return new ResponseEntity(new ExffMessage("Cannot create relationship request"), HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity(relationshipEntities, HttpStatus.OK);
     }
 
     @PostMapping("/relationship")
@@ -45,12 +65,12 @@ public class RelationshipController {
             int userId = userEntity.getId();
             boolean res = relationshipServices.acceptAddRelationshipRequest(id, userId);
             if (res) {
-                return new ResponseEntity(new ExffMessage("Relationship request has been created"), HttpStatus.OK);
+                return new ResponseEntity(new ExffMessage("Relationship request has been accepted"), HttpStatus.OK);
             } else {
-                return new ResponseEntity(new ExffMessage("Cannot create relationship request"), HttpStatus.BAD_REQUEST);
+                return new ResponseEntity(new ExffMessage("Cannot accept relationship request"), HttpStatus.BAD_REQUEST);
             }
         } catch (Exception e) {
-            return new ResponseEntity(new ExffMessage("Cannot create relationship request"), HttpStatus.BAD_REQUEST);
+            return new ResponseEntity(new ExffMessage("Cannot accept relationship request"), HttpStatus.BAD_REQUEST);
         }
     }
 }
--- src/main/java/com/capstone/exff/services/RelationshipServices.java
+++ src/main/java/com/capstone/exff/services/RelationshipServices.java
@@ -1,7 +1,14 @@
 package com.capstone.exff.services;
 
+import com.capstone.exff.entities.RelationshipEntity;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
 public interface RelationshipServices {
     boolean sendAddRelationshipRequest(int senderId, int receiverId);
     boolean acceptAddRelationshipRequest(int id, int userId);
     boolean removeRelationship(int id);
+    List<RelationshipEntity> getAddRelationshipRequest(int userId, Pageable pageable);
 }
