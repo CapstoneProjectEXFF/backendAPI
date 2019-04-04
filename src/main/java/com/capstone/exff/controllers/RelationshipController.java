76d0f8117c0d854bf0a414f40daa9576be311a23
decenova <maiducthang97@gmail.com>
2019-04-04 21:12:12 +0700

    fix relationship controller

--- src/main/java/com/capstone/exff/controllers/RelationshipController.java
+++ src/main/java/com/capstone/exff/controllers/RelationshipController.java
@@ -182,22 +182,4 @@ public class RelationshipController {
         }
         return new ResponseEntity(new ExffMessage("Deleted"), HttpStatus.OK);
     }
-
-    @DeleteMapping("/relationship")
-    public ResponseEntity unfriend(ServletRequest servletRequest, @RequestBody Map<String, String> body) {
-        try {
-            int loginUserId = getLoginUserId(servletRequest);
-            int firstID = Integer.parseInt(body.get("firstID"));
-            int secondID = Integer.parseInt(body.get("secondID"));
-            RelationshipEntity relationshipEntity = relationshipServices.getFriendRelationshipByUserId(firstID, secondID);
-            if (loginUserId == relationshipEntity.getReceiverId() || loginUserId == relationshipEntity.getSenderId()) {
-                relationshipServices.deleteRelationship(relationshipEntity);
-            } else {
-                return new ResponseEntity(new ExffMessage("Not permission"), HttpStatus.FORBIDDEN);
-            }
-        } catch (Exception e) {
-            return new ResponseEntity(new ExffMessage(e.getMessage()), HttpStatus.CONFLICT);
-        }
-        return new ResponseEntity(new ExffMessage("Deleted"), HttpStatus.OK);
-    }
 }
